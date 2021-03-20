216d2e8ba173ccb26bedc547100019b1b97c31a3
Nikhitha Gollamudi <56774561+ngoll@users.noreply.github.com>
2021-03-20 08:54:01 -0500

    getWordsListByFrequency
    Updated crypto.py with  getWordsListByFrequency

--- crypto.py
+++ crypto.py
@@ -38,7 +38,31 @@ class QuotesList:
    #this method returns the words in the quotes sorted by frequency
    #author: Nikhitha
    def getWordListByFrequency():
-       pass
+        file = open(file_name)
+        lines_list = file.readlines()
+        list_of_quotes = []
+        words_list = []
+        word_dict = {}
+        for x in lines_list:
+            y = x.split(",")    
+            list_of_quotes.append(y[1])
+     
+        for z in list_of_quotes:
+            words_list += z.split(" ")
+          
+        for word in words_list:
+            if word != "":
+                if word not in word_dict.keys():
+                   word_dict[word] = 1
+                else:
+                    word_dict[word] = word_dict.get(word) + 1
+        sorted_dict = {}
+        sorted_keys = sorted(word_dict, key=word_dict.get, reverse=True)  
+
+        for w in sorted_keys:
+            sorted_dict[w] = word_dict[w]
+
+        return sorted_dict
         
 
 
